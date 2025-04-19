4fd13e3d7957ad37ecaab736639b6e2cdd7df27d
Marko <auc_04@proton.me>
2025-04-19 23:50:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,29 @@ int main()
 {
     try
     {
-        constexpr int SZ = 17;
-        const char* input = {"message123456789"};
+        const char* input = {"super secret message1234566789"};
         const char* key = {"keykeykeykeykeyk"};
-        __uint8T out[SZ], rev[SZ];
+        const __uint64T byte_size = AESCrypto::getByteSize(input);
+         
 
-       std::cout << "Original Text(Hex):  ";
-       for(int i = 0; i < SZ; ++i) 
+       std::cout << "Original  Text(Hex)(" << std::dec << (int)byte_size << "): ";
+       for(int i = 0; i < byte_size; ++i) 
        std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)input[i] << " ";
        std::cout << std::endl;
 
-       AESCrypto::AES_Encryption<128> aes(input, out, key);
-       std::cout << "Encrypted Text(Hex): ";
-       for(int i = 0; i < SZ; ++i) 
-       std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)out[i] << " ";
+       AESCrypto::AES_Encryption<128> encryption(input, key);
+       Sequence<__uint8T> _out = encryption.invoke();
+       std::cout << "Encrypted Text(Hex)(" <<std::dec << (int)_out.size << "): ";
+       for(int i = 0; i < _out.size; ++i) 
+       std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)_out[i] << " ";
+       std::cout << std::endl;
+       AESCrypto::AES_Decryption<128> decryption(reinterpret_cast<__ccptrT>(_out.data), key);
+       Sequence<__uint8T> _rev = decryption.invoke();
+       std::cout << "Decrypted Text(Hex)(" << std::dec << (int)_rev.size << "): ";
+       for(int i = 0; i < _rev.size; ++i) 
+       std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)_rev[i] << " ";
        std::cout << std::endl;
 
-       AESCrypto::AES_Decryption<128> aesDec(reinterpret_cast<__ccptrT>(out), rev, key);
-       std::cout << "Decrypted Text(Hex): ";
-       for(int i = 0; i < SZ; ++i) 
-       std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)rev[i] << " ";
-       std::cout << std::endl;
 
     }
     catch (const std::exception &e)
