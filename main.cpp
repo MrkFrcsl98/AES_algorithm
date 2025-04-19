9193c108bdbd60a2a2c082301e273c94b1b44d00
Marko <auc_04@proton.me>
2025-04-19 23:54:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main()
 {
     try
     {
-        const char* input = {"super secret message1234566789"};
-        const char* key = {"keykeykeykeykeyk"};
+        const char* input = {"super secret message 0123456789"};
+        const char* key = {"keykeykeykeykeykkeykeykeykeykeyk"};
         const __uint64T byte_size = AESCrypto::getByteSize(input);
          
 
@@ -16,13 +16,15 @@ int main()
        std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)input[i] << " ";
        std::cout << std::endl;
 
-       AESCrypto::AES_Encryption<128> encryption(input, key);
+       constexpr __uint16T KEY_SIZE = 128;
+
+       AESCrypto::AES_Encryption<KEY_SIZE> encryption(input, key);
        Sequence<__uint8T> _out = encryption.invoke();
        std::cout << "Encrypted Text(Hex)(" <<std::dec << (int)_out.size << "): ";
        for(int i = 0; i < _out.size; ++i) 
        std::cout << std::setw(2) << std::hex << std::setfill('0') << (int)_out[i] << " ";
        std::cout << std::endl;
-       AESCrypto::AES_Decryption<128> decryption(reinterpret_cast<__ccptrT>(_out.data), key);
+       AESCrypto::AES_Decryption<KEY_SIZE> decryption(reinterpret_cast<__ccptrT>(_out.data), key);
        Sequence<__uint8T> _rev = decryption.invoke();
        std::cout << "Decrypted Text(Hex)(" << std::dec << (int)_rev.size << "): ";
        for(int i = 0; i < _rev.size; ++i) 
