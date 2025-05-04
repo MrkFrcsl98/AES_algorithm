858829b70756d05e26ac40e83d0f651d8a366875
Marko <auc_04@proton.me>
2025-05-04 16:44:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
-#include "aes.hpp" // the implementation of aes
 
-int main() {
-  try {
+#include "aes.hpp"      // include this for aes module
+#include "aes-test.hpp" // and this for testing
 
-    AesCryptoModule::Test::runGlobal();
-    
-    return 0;
-  } catch (const std::exception &e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
+int main(int argc, char **argv)
+{
+    try
+    {
+
+        AESTest::runGlobal();              // execute aes in all modes and all key sizes 
+
+        // AESTest::run_AES_ECB_test();    // execute aes only in ECB mode and all key sizes(128, 192, 256)
+        // AESTest::run_AES_CBC_test();    // execute aes only in CBC mode and all key sizes(128, 192, 256)
+        // AESTest::run_AES_OFB_test();    // execute aes only in OFB mode and all key sizes(128, 192, 256)
+        // AESTest::run_AES_CFB_test();    // execute aes only in CFB mode and all key sizes(128, 192, 256)
+        // AESTest::run_AES_CTR_test();    // execute aes only in CTR mode and all key sizes(128, 192, 256)
+        
+
+        return 0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
 }
